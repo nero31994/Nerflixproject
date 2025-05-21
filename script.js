fcdbfb6c3c8eca137a43d071bd519c8586335a30
nero31994 <arenaofvalorph937@gmail.com>
2025-05-21 14:48:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const API_KEY = '488eb36776275b8ae18600751059fb49';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const MOVIE_PROXY = 'https://nerflixprox.arenaofvalorph937.workers.dev/proxy?id=';
-const TV_PROXY = 'https://vidsrc.cc/v2/embed/tv/';
+const MOVIE_PROXY = 'https://vidora.su/movie/';
+const TV_PROXY = 'https://vidora.su/tv/';
 
 let currentPage = 1;
 let currentQuery = '';
@@ -15,7 +15,7 @@ async function fetchContent(query = '', page = 1) {
   document.getElementById("loading").style.display = "block";
 
   let endpoint = currentMode === 'movie' 
-    ? (query ? `search/movie` : `movie/popular`)
+    ? (query ? `search/movie` : `movie/popular`) 
     : (query ? `search/tv` : `tv/popular`);
 
   const url = `https://api.themoviedb.org/3/${endpoint}?api_key=${API_KEY}&query=${query}&page=${page}`;
@@ -64,12 +64,12 @@ function openModal(item) {
   document.getElementById("modalOverview").innerText = item.overview;
   document.getElementById("modalRelease").innerText = `Release: ${item.release_date || item.first_air_date || 'N/A'}`;
   document.getElementById("modalRating").innerText = `Rating: ${item.vote_average || 'N/A'}`;
-  
+
   const iframe = document.getElementById("videoFrame");
   if (currentMode === 'movie') {
     iframe.src = `${MOVIE_PROXY}${item.id}`;
   } else {
-    iframe.src = `${TV_PROXY}${item.id}?autoPlay=false`;
+    iframe.src = `${TV_PROXY}${item.id}`;
   }
 
   document.getElementById("movieModal").style.display = "flex";
